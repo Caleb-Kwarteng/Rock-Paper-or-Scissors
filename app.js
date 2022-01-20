c049ec7fb9573297298efc964157301d44244fb5
Caleb Buabeng Kwarteng <caleb.kwarteng@amalitech.org>
2022-01-20 20:40:00 +0000

    Create app.js
    Rock, paper,scissor

---
+++ app.js
@@ -0,0 +1,77 @@
+const getUserChoice = userInput  => {
+
+    userInput = userInput.toLowerCase();
+  
+    if(userInput === 'rock' || userInput === 'paper' || userInput === 'scissors')
+    {
+      return userInput;
+    }
+    else
+    {
+      console.log('Error!');
+    }
+  }
+  
+  const getComputerChoice = () => {
+    randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 3:
+      return 'scissors';
+  }
+  }
+  
+  const determineWinner = (userChoice, computerChoice) => {
+    if(userChoice == computerChoice){
+      return "Draw Game";
+    }
+    if(userChoice === 'rock'){
+      if(computerChoice === 'paper'){
+        return 'The computer won!';
+      }
+      else{
+        return 'You won';
+      }
+    }
+    if(userChoice === 'paper'){
+      if(computerChoice === 'scissors'){
+        return 'The computer won';
+      }
+      else{
+        return 'You won!';
+      }
+    }
+    if (userChoice === 'scissors') {
+    if (computerChoice === 'rock') {
+      return 'The computer won!';
+    } else {
+      return 'You won!';
+    }
+  }
+  }
+  
+  const playGame = () => {
+    const userChoice = getUserChoice('rock');
+    const computerChoice = getComputerChoice();
+     console.log('You threw: ' + userChoice);
+    console.log('The computer threw:' + computerChoice);
+    console.log(determineWinner(userChoice, computerChoice));
+  }
+  
+  playGame();
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
